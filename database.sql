154635b709093a2ca65c5358ecca6cc748ec1d2e
Hezron Nyamboga <hezzyn1@gmail.com>
2024-07-09 16:45:30 +0300

    create products -- sql

--- database.sql
+++ database.sql
@@ -7,3 +7,36 @@ CREATE TABLE users (
 );
 
 ALTER TABLE users ADD CONSTRAINT email_format_check CHECK (email ~* '^[^@]+@[^@]+\.[^@]{2,}$');
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price NUMERIC DEFAULT 0 NOT NULL,
+    discount NUMERIC DEFAULT 0,
+    description VARCHAR(1000) NOT NULL,
+    image VARCHAR DEFAULT '/uploads/example.jpeg',
+    category VARCHAR NOT NULL CHECK (category IN ('office', 'kitchen', 'bedroom' , 'others')),
+    company VARCHAR NOT NULL CHECK (company IN ('ikea', 'liddy', 'marcos', 'others')),
+    colors TEXT[] DEFAULT ARRAY['#222']::TEXT[] NOT NULL,
+    featured BOOLEAN DEFAULT FALSE,
+    freeShipping BOOLEAN DEFAULT FALSE,
+    inventory INTEGER DEFAULT 15 NOT NULL,
+    averageRating NUMERIC DEFAULT 0,
+    numOfReviews INTEGER DEFAULT 0,
+    "user" INTEGER NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY("user") REFERENCES users(id)
+);
+
+-- Create a trigger to update the updated_at column on update
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = NOW();
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_products_updated_at BEFORE UPDATE
+ON products FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
